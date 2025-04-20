156df7bf75871a9272d423e10f50d2d8171a3271
royalcourtbd <sayedvai08@gmail.com>
2025-04-21 01:15:19 +0600

    হোম প্রেজেন্টার, ইউআই স্টেট এবং হোম পৃষ্ঠা ফাইলগুলি মুছে ফেলা হয়েছে। প্রেজেন্টার সেটআপ থেকে হোম প্রেজেন্টারের নিবন্ধন অপসারণ করা হয়েছে, যা পরিষ্কার আর্কিটেকচার অনুসরণ করে।

--- lib/core/di/setup/presenter_setup.dart
+++ lib/core/di/setup/presenter_setup.dart
@@ -2,7 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:initial_project/core/base/base_presenter.dart';
 import 'package:initial_project/core/di/service_locator.dart';
 import 'package:initial_project/core/di/setup/setup_module.dart';
-import 'package:initial_project/presentation/home/presenter/home_presenter.dart';
 
 import 'package:initial_project/presentation/product/presenter/product_details_presenter.dart';
 import 'package:initial_project/presentation/product/presenter/product_list_presenter.dart';
@@ -14,7 +13,6 @@ class PresenterSetup implements SetupModule {
   @override
   Future<void> setup() async {
     _serviceLocator
-      ..registerLazySingleton(() => loadPresenter(HomePresenter()))
       ..registerLazySingleton(
         () => loadPresenter(
           ProductListPresenter(locate(), locate(), locate(), locate()),
